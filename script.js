803809d3ebbbf77b5de00cf989b68df1cccc9175
ninizedginidze <nino.zedginidze.1@iliauni.edu.ge>
2025-05-27 21:11:55 +0400

    change age value

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let age='hgsahg';
+let age=23;
  if (age<18){ console.log ('თქვენ არ შეგიძლიათ მართვის მოწმობის აღება');
  }
  else if (age>=18){console.log ('თქვენ შეგიძლიათ მართვის მოწმობის აღება');
